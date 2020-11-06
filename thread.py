77df0945041e7e3d87c56c1dab67ffb5e444da52
igortereshchenko <tereshchenko.igor@gmail.com>
2020-11-06 14:00:48 +0200

    thread

---
+++ thread.py
@@ -0,0 +1,38 @@
+
+# import  threading
+import random
+import time
+from threading import Thread
+
+class Resourse:
+
+    def __init__(self):
+        self.value = 1
+
+class NumberPrinter(Thread):
+
+    def __init__(self, thread_id, resourse):
+        # init smth for thread
+        Thread.__init__(self)
+        # your job
+        self.thread_id = thread_id
+        self.resourse = resourse
+
+    def run(self):
+
+        number = random.randint(1,5)
+        time.sleep(number)
+
+
+        self.resourse.value = self.resourse.value+1
+        print("thread", self.name, "is running with number", number, "value", self.resourse.value)
+
+#  RUN FEW THREADS
+
+resource = Resourse()
+for i in range(1000):
+    thread = NumberPrinter(i,resource)
+    thread.start()
+    # thread.join()
+
+# print("Finish:", resource.value)
